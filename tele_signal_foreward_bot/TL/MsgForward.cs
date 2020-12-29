90521c4d6741546417fffc4415fcd9faa68df62f
skuk <skuk@gridwiz.com>
2020-12-29 18:14:06 +0900

    updating...

--- tele_signal_foreward_bot/TL/MsgForward.cs
+++ tele_signal_foreward_bot/TL/MsgForward.cs
@@ -34,19 +34,141 @@ namespace tele_signal_foreward_bot.TL
 
         public async Task<Task> Send(List<TLMessage> messages, Dictionary<int, int> MessageMap, TLChannel channel, Dictionary<int, string> MessageCheckMap)
         {
+            var config = Config.Configs.Instance;
+            foreach (var message in messages)
+            {
+                var messageId = message.Id;
+                if( messageId > MessageMap[channel.Id])
+                {
+                    string caption = GetCaption(message, channel);
+
+                    if (message.Media != null)
+                    {
+                        if (message.Media is TeleSharp.TL.TLMessageMediaPhoto)
+                        {
+                            string fileName = SavePhoto(message.Media as TeleSharp.TL.TLMessageMediaPhoto);
+                            
+                            if(fileName != "")
+                            {
+                                var fileResult = GetFile(fileName).Result;
+                                // 중복전송 방지
+                                if ( caption.Contains(MessageCheckMap[channel.Id]) 
+                                     || MessageCheckMap[channel.Id].Contains(caption))
+                                {
+                                    
+                                }
+                                else
+                                {
+                                    var updates = await Client.SendUploadedPhoto(new TLInputPeerChannel() { ChannelId = Receiver.Instance.ForwardChannelId, AccessHash = Receiver.Instance.ForwardChannelAccessHash },
+                                    fileResult,
+                                    caption);
+                                }
+
+                            }
+                            else
+                            {
+                                await Client.SendMessageAsync(new TLInputPeerChannel() { ChannelId = Receiver.Instance.ForwardChannelId, AccessHash = Receiver.Instance.ForwardChannelAccessHash }, caption);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        await Client.SendMessageAsync(new TLInputPeerChannel() { ChannelId = Receiver.Instance.ForwardChannelId, AccessHash = Receiver.Instance.ForwardChannelAccessHash }, caption);
+                    }
+
+                    MessageMap[channel.Id] = messageId;
+                    if (MessageCheckMap.ContainsKey(channel.Id))
+                    {
+                        MessageCheckMap[channel.Id] = caption;
+                    }
+                    else
+                    {
+                        MessageCheckMap.Add(channel.Id, caption);
+                    }
+                }
+
+
+            }
+                
             return Task.CompletedTask;
         }
-
-
-        void SavePhoto(TeleSharp.TL.TLMessageMediaPhoto photoMSg)
+        /// <summary>
+        /// 텔레그램에 보낼 사진파일 준비단계. 
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        async Task<TLAbsInputFile> GetFile(string fileName) 
         {
-
+            string path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+            var fileResult = /*(TLInputFile)*/await Client.UploadFile(fileName, new StreamReader(path));
+            return fileResult; 
         }
 
+        /// <summary>
+        /// 텔레그램 메세지에 있는 사진 파일 저장. 
+        /// </summary>
+        /// <param name="photoMsg"></param>
+        /// <returns></returns>
+        string SavePhoto(TeleSharp.TL.TLMessageMediaPhoto photoMsg)
+        {
+            var photo = ((TLPhoto)photoMsg.Photo);
+            var photoSize = photo.Sizes.OfType<TLPhotoSize>().OrderByDescending(m => m.Size).FirstOrDefault();
+            TLFileLocation tf = (TLFileLocation)photoSize.Location;
+            string fileName = $"{photo.Id}.jpg";
+            string path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            try
+            {
+                var mb = 1048576;
+                var upperLimit = (int)Math.Pow(2, Math.Ceiling(Math.Log(photoSize.Size, 2))) * 4;
+                var limit = Math.Min(mb, upperLimit);
+                var currentOffset = 0;
+
+                using (var fs = File.OpenWrite(path))
+                {
+                    while (currentOffset < photoSize.Size)
+                    {
+                        TLFile file = Client.GetFile(new TLInputFileLocation { LocalId = tf.LocalId, Secret = tf.Secret, VolumeId = tf.VolumeId }, limit, currentOffset).ConfigureAwait(false).GetAwaiter().GetResult();
+                        fs.Write(file.Bytes, currentOffset, file.Bytes.Length);
+                        currentOffset += file.Bytes.Length;
+                    }
+
+                    fs.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($" getFile (size:{photoSize.Size}) : {ex.Message} \n {ex.StackTrace}");
+                return "";
+                
+            }
+
+            return fileName;
+        }
+
+        string GetCaption(TLMessage msg, TLChannel channel)
+        {
+            string caption = "";
+            DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, System.DateTimeKind.Utc);
+            DateTime dt = dtDateTime.AddSeconds(msg.Date);
+            if (msg.Media != null)
+            {
+                caption = ((TLMessageMediaPhoto)msg.Media).Caption;
+                
+                if(!string.IsNullOrEmpty(caption))
+                    caption += $"\n\n FROM {channel.Title}";
+            }
+            else
+            {
+                caption = msg.Message;
+                caption += $"\n\n FROM {channel.Title}";
+            }
+
+            return caption;
+        }
         void UpdateMessageMap(Dictionary<int, int> MessageMap, Dictionary<int, string> MessageCheckMap, string msg, int msgNum)
         {
-            // 비트상승 > 알트하방 > 비트하방 > 알트더하방 > 비트 반등 > 알트롱  > 비트저항 > 알트조정 (알트매수타임)
-            // 비트 하방 > 알트 상방 > 비트 상방 > 알트더 상방 > 비트 저항 > 알트숏  > 비트상승 > 알트 롱(알트 숏타임) 
+            
         }
 
     }
